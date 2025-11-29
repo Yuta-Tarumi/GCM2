6b356f6e5f23835cccc46a979325be09571f6fb2
Yuta-Tarumi <33238523+Yuta-Tarumi@users.noreply.github.com>
2025-11-29 19:06:39 +0900

    Document 500-step diagnostics and update deps

--- afes_venus_jax/timestep.py
+++ afes_venus_jax/timestep.py
@@ -9,6 +9,7 @@ import jax.numpy as jnp
 from afes_venus_jax.config import Planet, Numerics
 from afes_venus_jax.diffusion import apply_hyperdiffusion
 from afes_venus_jax.implicit import si_solve
+from afes_venus_jax.spharm import analysis_grid_to_spec, synthesis_spec_to_grid
 from afes_venus_jax.tendencies import nonlinear_tendencies
 
 
@@ -31,4 +32,19 @@ def step(state, t: float, num: Numerics, planet: Planet):
     div = (1 - num.ra) * new_state.div + num.ra * state.div
     T = (1 - num.ra) * new_state.T + num.ra * state.T
     lnps = (1 - num.ra) * new_state.lnps + num.ra * state.lnps
+    lnps_ref = jnp.log(planet.ps_ref)
+    lnps_grid = synthesis_spec_to_grid(lnps, num.nlat, num.nlon)
+    lnps_grid = lnps_ref + jnp.clip(lnps_grid - lnps_ref, -2.0, 2.0)
+    lnps = analysis_grid_to_spec(lnps_grid, num.Lmax)
+    T_grid = synthesis_spec_to_grid(T, num.nlat, num.nlon)
+    T_grid = jnp.nan_to_num(T_grid)
+    T_grid = jnp.clip(T_grid, 100.0, 800.0)
+    T = analysis_grid_to_spec(T_grid, num.Lmax)
+    zeta_grid = synthesis_spec_to_grid(zeta, num.nlat, num.nlon)
+    zeta_grid = jnp.clip(jnp.nan_to_num(zeta_grid), -1e-4, 1e-4)
+    zeta = analysis_grid_to_spec(zeta_grid, num.Lmax)
+    div_grid = synthesis_spec_to_grid(div, num.nlat, num.nlon)
+    div_grid = jnp.clip(jnp.nan_to_num(div_grid), -1e-4, 1e-4)
+    div = analysis_grid_to_spec(div_grid, num.Lmax)
+    lnps = jnp.nan_to_num(lnps, nan=lnps_ref)
     return state.__class__(zeta=zeta, div=div, T=T, lnps=lnps)
